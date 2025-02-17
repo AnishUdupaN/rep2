b17b4c95e200ad95b8dbe6a7bd438110a36ddba7
AnishUdupaN <anishudupa2005@gmail.com>
2025-02-17 09:49:45 +0000

    added per_sq.py file

---
+++ per_sq.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number: "))
+if (int(num**(1/2))==num**(1/2)):
+    print("The given number is a perfect square.")
+else:
+    print("The given number is not a perfect square.")
