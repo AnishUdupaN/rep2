0d2c776ac4671d5f6a5edab89f7ef46f3473fe1f
anonymous <anonymous>
2025-02-20 12:13:38 +0530

    added a program to find the sum of oddly placed even integers in a given input

--- odd_even_integers.py
+++ odd_even_integers.py
@@ -1,10 +1,8 @@
-numsum=0
-num=list(map(int,input('Enter a Number: ').split()))
-#num=list(input('Enter a Number: ').split())
-for i in range(len(num)):
-    if i%2==0 and num[i]%2==0:
-        numsum+=num[i]
-        print(numsum,end='  ')
-
-print()
-#print(numsum)
+nums=list(map(int,input("Enter the numbers: ").split()))
+#nums=[int(i) for i in input("Enter the numbers: ").split()]
+numlist=[]
+for i in range(len(nums)):
+    if i%2==0 and nums[i]%2==0:
+        numlist.append(nums[i])
+print(numlist)
+print(sum(numlist))
