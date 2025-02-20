04870154bbf34f9b16b3cc03adeecc7abfcc6ec4
anonymous <anonymous>
2025-02-20 12:04:24 +0530

    changed insertion_sort_on_strings.py

--- insertion_sort_on_strings.py
+++ insertion_sort_on_strings.py
@@ -1,4 +1,4 @@
-string=input("Enter the string:")
+'''string=input("Enter the string:")
 def insertion_sort_string(s):
     chars=list(s)
     for i in range(1, len(chars)):
@@ -9,5 +9,20 @@ def insertion_sort_string(s):
             j-=1
         chars[j+1]=key
     return ''.join(chars)
+sorted_string=insertion_sort_string(string)
+print("Sorted string:",sorted_string)'''
+
+
+
+string=input("Enter the string:").split()
+def insertion_sort_string(chars):
+    for i in range(1, len(chars)):
+        key=chars[i]
+        j=i-1
+        while j>=0 and key<chars[j]:
+            chars[j+1]=chars[j]
+            j-=1
+        chars[j+1]=key
+    return chars
 sorted_string=insertion_sort_string(string)
 print("Sorted string:",sorted_string)
